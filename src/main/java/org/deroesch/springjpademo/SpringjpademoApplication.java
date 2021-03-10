96212c42f88717bd8b56586f3c80bbb15db07f3c
Douglas Roesch <Douglas.Roesch@bnymellon.com>
2021-03-10 15:34:26 -0500

    General cleanup

--- src/main/java/org/deroesch/springjpademo/SpringjpademoApplication.java
+++ src/main/java/org/deroesch/springjpademo/SpringjpademoApplication.java
@@ -3,11 +3,15 @@ package org.deroesch.springjpademo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class SpringjpademoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringjpademoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        log.info("Starting");
+        SpringApplication.run(SpringjpademoApplication.class, args);
+    }
 
 }
